9b1856e7709e651a2fccc62c92da18fe3aacbda7
Xeno <xenophilicy@gmail.com>
2020-11-02 10:51:38 -0600

    Added End Crystal entity

---
+++ src/Xenophilicy/Smaccer/entities/SmaccerEnderCrystal.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Xenophilicy\Smaccer\entities;
+
+/**
+ * Class SmaccerEnderCrystal
+ * @package Xenophilicy\Smaccer\entities
+ */
+class SmaccerEnderCrystal extends SmaccerEntity {
+    
+    const TYPE_ID = 71;
+    const HEIGHT = 2;
+    
+}
